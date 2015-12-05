21b7e22f837f04cce750c542377645a2218b1909
Melvin <melvin0008@gmail.com>
2015-12-04 19:17:55 -0500

    changed app.py for structure

--- app.py
+++ app.py
@@ -1,6 +1,25 @@
 #!venv/bin/python
 
+import requests
+from flask import Flask, jsonify
+from flask import abort
+from flask import make_response
+from flask import request
+from flask import url_for
+
 app = Flask(__name__)
 
-@app.route('/')
-	def index():
+@app.route('/search/<string:query>', methods=['GET'])
+	def search(query):
+		return 
+
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
