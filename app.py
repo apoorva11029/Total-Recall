ed58d60b699051adb21060a2571108cf9f0a2ea6
Melvin <melvin0008@gmail.com>
2015-12-05 15:02:22 -0500

    frontend+backend working

--- app.py
+++ app.py
@@ -6,14 +6,14 @@ from flask import abort
 from flask import make_response
 from flask import request
 from flask import url_for
+from flask.ext.cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/search/<string:query>', methods=['GET'])
-	def search(query):
-		return 
-
-
+def search(query):
+	return make_response(jsonify({'name': query+"shit"}))
 
 @app.errorhandler(404)
 def not_found(error):
