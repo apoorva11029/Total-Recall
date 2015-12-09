e82857b3e4607a9e7b31189da3024742b3e2e7d6
Melvin <melvin0008@gmail.com>
2015-12-09 09:19:20 -0500

    changed return dictstructure

--- app.py
+++ app.py
@@ -16,35 +16,61 @@ from textblob import TextBlob
 app = Flask(__name__)
 CORS(app)
 
+
 @app.route('/search/<string:query>', methods=['GET'])
 def search(query):
-	#gs = goslate.Goslate()
-	#language_id = gs.detect(query)
-	#print gs.get_languages()[language_id] #German
-	#print language_id #de	='
-	# Example Query : http://athigale.koding.io:8983/solr/projc/select?defType=dismax&q=*attack*&qf=text_en^3+tweet_hashtags^2+keywords^2&wt=json
 	b = TextBlob(u""+query+"")
-	language_id = b.detect_language()	
+	language_id = b.detect_language()
 	connection = urlopen('http://athigale.koding.io:8983/solr/projc/select?defType=dismax&q=*'+query+'*&qf=text_'+ language_id +'^1+hashtags^1+concept^0.1+keywords^1&wt=json')
 	response = simplejson.load(connection)
-	#print response['response']['numFound'], "documents found."
-	#print response 
-	#for document in response['response']['docs']:
-  	#	print document['text_en']		
-	return make_response(jsonify(response))	
+	lang="".join('text_'+ language_id)
+	# print (response['response']['docs'][0][])
+	# print response[0]['text_en']
+	returnArr={}
+	locations=[]
+	tweets=[]
+	for tweet in response['response']['docs']:
+		tempd={}
+		tempd['text']==tweet[lang]
+		# tempd['picid']=tweet['']
+		# tempd['screenhandle']=tweet
+		tweets.append(tempd)
+		locations.append({tweets['locationCoordinates'][0],tweets['locationCoordinates'][1]})
+	returnArr['tweets']=tweets
+	returnArr['locations']=locations
+	#returnArr['people']=people
+	#returnArr['relate_terms']=relatedterms
+	return make_response(jsonify(response))
+
+# @app.route('/search/<string:query>', methods=['GET'])
+# def search(query):
+# 	#gs = goslate.Goslate()
+# 	#language_id = gs.detect(query)
+# 	#print gs.get_languages()[language_id] #German
+# 	#print language_id #de	='
+# 	# Example Query : http://athigale.koding.io:8983/solr/projc/select?defType=dismax&q=*attack*&qf=text_en^3+tweet_hashtags^2+keywords^2&wt=json
+# 	b = TextBlob(u""+query+"")
+# 	language_id = b.detect_language()	
+# 	connection = urlopen('http://athigale.koding.io:8983/solr/projc/select?defType=dismax&q=*'+query+'*&qf=text_'+ language_id +'^1+hashtags^1+concept^0.1+keywords^1&wt=json')
+# 	response = simplejson.load(connection)
+# 	#print response['response']['numFound'], "documents found."
+# 	#print response
+# 	#for document in response['response']['docs']:
+#   	#	print document['text_en']	
+# 	return make_response(jsonify(response))	
 	#return make_response(jsonify({'name': query+"shit"}))
 
-@app.route('/search2/<string:query>', methods=['GET'])
-def search2(query):
-	gs = goslate.Goslate()
-	language_id = gs.detect(query)
-	#print gs.get_languages()[language_id] #German
-	#print language_id #de	
-	# create a connection to a solr server
-	s = solr.SolrConnection('http://athigale.koding.io:8983/solr/projc/')
-	# do a search
-	response = s.query('hashtags:'+query)
-	return make_response(jsonify(response))
+# @app.route('/search2/<string:query>', methods=['GET'])
+# def search2(query):
+# 	gs = goslate.Goslate()
+# 	language_id = gs.detect(query)
+# 	#print gs.get_languages()[language_id] #German
+# 	#print language_id #de	
+# 	# create a connection to a solr server
+# 	s = solr.SolrConnection('http://athigale.koding.io:8983/solr/projc/')
+# 	# do a search
+# 	response = s.query('hashtags:'+query)
+# 	return make_response(jsonify(response))
 
 @app.errorhandler(404)
 def not_found(error):
