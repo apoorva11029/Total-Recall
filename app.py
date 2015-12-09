c41ae7c1f71e078ffcc776d3b1f592fa3814a391
Melvin <melvin0008@gmail.com>
2015-12-08 22:01:05 -0500

    added requirements.txt

--- app.py
+++ app.py
@@ -7,13 +7,40 @@ from flask import make_response
 from flask import request
 from flask import url_for
 from flask.ext.cors import CORS
+from urllib2 import *
+import simplejson
+import goslate
+import solr
 
 app = Flask(__name__)
 CORS(app)
 
 @app.route('/search/<string:query>', methods=['GET'])
 def search(query):
-	return make_response(jsonify({'name': query+"shit"}))
+	gs = goslate.Goslate()
+	language_id = gs.detect(query)
+	#print gs.get_languages()[language_id] #German
+	print language_id #de	
+	connection = urlopen('http://athigale.koding.io:8983/solr/projc/select?q=*'+query+'*&wt=json')
+	response = simplejson.load(connection)
+	#print response['response']['numFound'], "documents found."
+	#print response 
+	#for document in response['response']['docs']:
+  	#	print document['text_en']		
+	return response	
+	#return make_response(jsonify({'name': query+"shit"}))
+
+@app.route('/search2/<string:query>', methods=['GET'])
+def search(query):
+	gs = goslate.Goslate()
+	language_id = gs.detect(query)
+	#print gs.get_languages()[language_id] #German
+	print language_id #de	
+	# create a connection to a solr server
+	s = solr.SolrConnection('http://athigale.koding.io:8983/solr/projc/')
+	# do a search
+	response = s.query('hashtags:'+query)
+	return response
 
 @app.errorhandler(404)
 def not_found(error):
